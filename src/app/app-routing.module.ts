63b905500abaf47afd78a0185c934a34dfee3317
Chelluri Vandhana <vchelluri@corp.miraclesoft.com>
2023-05-17 00:28:57 +0530

    trpc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+
+  { path: 'first', component: FirstComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { StudentStandAloneComponent } from './student-stand-alone/student-stand-alone.component';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[StudentStandAloneComponent]
 })
 export class AppComponent {
   title = 'Angulardemo';
+  data: any;
+  constructor(){
+
+  }
 }
